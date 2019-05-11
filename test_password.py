8b9b34a0b913fa1c7346074651f4b8cd59e133e4
gichimux <gichimumwai1@gmail.com>
2019-05-11 19:44:10 +0300

    test of credentials class passes

--- test_password.py
+++ test_password.py
@@ -1,23 +1,26 @@
 import unittest
 from password import Credentials
+
 class TestCredentials(unittest.TestCase):
     '''
     main test class for the credentials class
     '''
     def setUp(self):
         '''
-        runs before every test
+        Method that creates instances of class for testing
         '''
         
-        self.new_user = Credentials(1,"jane","doe")
+        self.new_credentials = Credentials(1,1,"jane","medium.com", '1234')
     
     def test_init(self):
         '''
-        test if pbject is instantiated correctly
+        test if object is instantiated correctly
         '''
-        self.assertEqual(self.new_user.id, 1)
-        self.assertEqual(self.new_user.first_name, "jane")
-        self.assertEqual(self.new_user.last_name, "doe")
+        self.assertEqual(self.new_credentials.user_id, 1)
+        self.assertEqual(self.new_credentials.credentials_id, 1)
+        self.assertEqual(self.new_credentials.user_name, "jane")
+        self.assertEqual(self.new_credentials.app_name, "medium.com")
+        self.assertEqual(self.new_credentials.app_password, "1234")
 
 if __name__ == "__main__":
     unittest.main()
