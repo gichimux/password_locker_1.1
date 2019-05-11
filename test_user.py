c48f836dc5f5f7dd2e515874426b0f94278a5194
gichimux <gichimumwai1@gmail.com>
2019-05-11 21:51:48 +0300

    test for initialization of user class fails

---
+++ test_user.py
@@ -0,0 +1,25 @@
+import unittest
+from models import User
+ 
+class TestUser(unittest.TestCase):
+    '''
+    main test for User class
+    '''
+    def setUp(self):
+         '''
+         Method that creates a user Object for testing
+         '''
+         self.new_user = User(1, "cersei", "jon","cerjon", "x")
+
+    def test_init(self):
+        '''
+        test if object is instantiated correctly
+        '''
+        self.assertEqual(self.new_user.user_id, 1)
+        self.assertEqual(self.new_user.fname, "cersei")
+        self.assertEqual(self.new_user.lname, "jon")
+        self.assertEqual(self.new_user.user_name, "cerjon")
+        self.assertEqual(self.new_user.login_password, "x")
+
+if __name__ == "__main__":
+    unittest.main()
